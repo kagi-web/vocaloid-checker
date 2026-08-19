e87828a6abaa8c70f8850bf7c8e5a0685c3d5ab7
kagi <221946014+kagi-web@users.noreply.github.com>
2026-08-19 20:55:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -345,11 +345,10 @@ function shareOnX() {
         '#ボカロ曲知ってる率チェッカー';
 
 
-    const url =
-        "https://twitter.com/intent/tweet" +
-        `?text=${encodeURIComponent(text)}` +
-        `&url=${encodeURIComponent(location.href)}`;
-
+   const url =
+    "https://twitter.com/intent/tweet" +
+    `?text=${encodeURIComponent(text)}` +
+    `&url=${encodeURIComponent(location.href + "?s")}`;
 
     window.open(
         url,
